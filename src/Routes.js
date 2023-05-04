789a804a4be6363ff784335c24d7156d9fa135ad
Neeraj Gandhi <64327599+neeraj-22@users.noreply.github.com>
2023-05-04 21:05:42 +0530

    Init

---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import { BrowserRouter, Routes as RoutesAlt, Route } from "react-router-dom";
+// import Homepage from "./pages/Homepage";
+
+// const Routes = () => {
+//   return (
+//     <BrowserRouter>
+//       <RoutesAlt>
+//         <Route path="/" exact element={<Homepage />} />
+//       </RoutesAlt>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default Routes;
